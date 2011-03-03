257089c4f2ddf415a0afd5e652b5a7d023d69f42
adiglucksam@gmail.com <adiglucksam@gmail.com@0f038785-6cf4-3125-cb35-955979e70f02>
2011-03-03 08:11:04 +0000

    bug fix...

--- src/tools/HelpfoulMathpud.java
+++ src/tools/HelpfoulMathpud.java
@@ -1,10 +1,14 @@
 package tools;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import saddleback_caterpillar.activity.Erorr;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelpfoulMathpud {
 	public  static void moveActivtyWitheParam(Class<?> subActivity,Activity fatherActivity,Map<String,String> parmters){
@@ -17,9 +21,19 @@ public class HelpfoulMathpud {
 		fatherActivity.startActivity(newIntent);
 		fatherActivity.finish();
 	}
-	public static void Error(Activity active) {
-		Intent newIntent = new Intent(active.getApplicationContext(), Error.class);
-		active.startActivity(newIntent);
-		active.finish();
-	}	
+	public static void Error(Activity active,String problemType ) {
+		Map<String,String> parmters = new HashMap<String, String>();
+		String problem = "";
+		if(problemType.equals("NO_CONNECTION") ){
+			problem = "We have comunication problems, please try again later";
+		}else if(problemType.equals("BAD_BALLOT")){
+			problem = "Your Ballot is corrupted";
+		}else if(problemType.equals("REINSTALL")){
+			problem = "Your program is corrupted, please reinstall";
+		}else{
+			problem = "An unknown error has occured, please try to restart your android";
+		}
+		parmters.put("problem", problem);
+		moveActivtyWitheParam(Erorr.class , active ,parmters);
+	}
 }
