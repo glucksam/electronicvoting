dc6d9e3ae2c535f275af6b6c13f11fc93cf7b584
adiglucksam@gmail.com <adiglucksam@gmail.com@0f038785-6cf4-3125-cb35-955979e70f02>
2011-03-13 20:17:51 +0000

    (1) fixed portrait-landscape screen (2) fixed scanner problem (if needed to install, we close our program) (3) fixed spelling & messages

--- trunk/src/saddleback_caterpillar/activity/ElectronicVoting.java
+++ trunk/src/saddleback_caterpillar/activity/ElectronicVoting.java
@@ -8,6 +8,7 @@ import tools.IntentResult;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,13 +16,18 @@ import android.widget.Button;
 public class ElectronicVoting extends Activity {
 	private String I_sFromScan = null;
 	private HashMap<String, String> parameters;
+	private boolean isScannerNotInstalled = false; 
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		isScannerNotInstalled = false;
 		parameters = new HashMap<String, String>();
-		IntentIntegrator.initiateScan(this);	
+		 if(null != IntentIntegrator.initiateScan(this)){/*installing scanner*/
+			 Log.d("WORKSHOP", "scanner not installed!!!");
+			 isScannerNotInstalled = true;
+		 }
 	}
 
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -30,37 +36,53 @@ public class ElectronicVoting extends Activity {
 		if (scanResult == null) {
 			HelpfulMathods.Error(getActivityInstanc(), "BAD_BALLOT");
 			return;
-		}else if(scanResult.getContents() == null){/*need to install barcode scanner*/
-			/*TODO: take care of this case- when teh scanner is not installed*/
+		} else if (scanResult.getContents() == null) {/*
+													 * need to install barcode
+													 * scanner- this is user
+													 * mistake
+													 */
 			return;
-		}
-		else {
+		} else {
 			I_sFromScan = scanResult.getContents();
 		}
 		if (!parameters.containsKey("firstScanReasult")) {/* it's the first scan */
 			parameters.put("firstScanReasult", I_sFromScan);
-			setContentView(R.layout.isaudit);
-			Button yesButton = (Button) findViewById(R.id.Yes);
-			yesButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					IntentIntegrator.initiateScan(getActivityInstanc());
-				}
-			});
-			Button noButton = (Button) findViewById(R.id.No);
-			noButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					parameters.put("secondScanReasult", "");
-					HelpfulMathods.moveActivtyWitheParam(Verifying.class,
-							getActivityInstanc(), parameters);
-				}
-			});
+			presentingIaAuditScreen();
 		} else {/* second scan */
 			parameters.put("secondScanReasult", I_sFromScan);
 			HelpfulMathods.moveActivtyWitheParam(Verifying.class,
 					getActivityInstanc(), parameters);
 		}
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d("WORKSHOP", "onStop");
+		if(isScannerNotInstalled){
+			Log.d("WORKSHOP", "committing suicide");
+			this.finish();
+		}
+	}
+	
+	public void presentingIaAuditScreen() {
+		setContentView(R.layout.isaudit);
+		Button yesButton = (Button) findViewById(R.id.Yes);
+		yesButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				IntentIntegrator.initiateScan(getActivityInstanc());
+			}
+		});
+		Button noButton = (Button) findViewById(R.id.No);
+		noButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				parameters.put("secondScanReasult", "");
+				HelpfulMathods.moveActivtyWitheParam(Verifying.class,
+						getActivityInstanc(), parameters);
+			}
+		});
 	}
 
 	public Activity getActivityInstanc() {
